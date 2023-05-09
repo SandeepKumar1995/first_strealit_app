628021586063ef29aa31c3230347e774a8a3e008
SandeepKumar1995 <54086388+SandeepKumar1995@users.noreply.github.com>
2023-05-09 15:13:58 -0400

    Update streamlit_app.py
    Removed Breakfast items

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,4 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.heading('breakfast Menu')
 
-streamlit.text('Omega 3 and Blueberry oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard Boiled Free-Range Eggs')
+
